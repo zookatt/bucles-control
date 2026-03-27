ed8d8b472837c7e8c64fb146161b4a396409c584
zookat <zotova.ea@gmail.com>
2026-03-27 10:31:52 +0100

    Correcion de funciones

--- funciones-avanzadas-susana/script2.js
+++ funciones-avanzadas-susana/script2.js
@@ -22,13 +22,14 @@
 let nombre = prompt("Introduce el nombre del producto");
 let precio = Number(prompt("Introduce el precio del producto"));
 let cantidadTexto = prompt("Introduce la cantidad del producto");
-let cantidad = cantidadTexto === "" ? undefined : parseInt(cantidadTexto);
+//let cantidad = Number(prompt("Introduce la cantidad del producto"));
+let cantidad = cantidadTexto === "" ? 1 : parseInt(cantidadTexto);
 
 function calcular(precio, cantidad = 1) {
-  cantidad === "" ? undefined : parseInt(cantidadTexto);
   return precio * cantidad;
 }
+
 let resultado1 = calcular(precio, cantidad);
 alert(
-  `El total de la compra del producto ${nombre} con el precio/ud ${precio} y cantidad  ${cantidad} ud. es ${resultado1} euros`,
+  `El total de la compra del producto ${nombre} con el precio/ud ${precio} y cantidad  ${cantidad ?? 1} ud. es ${resultado1} euros`,
 );
